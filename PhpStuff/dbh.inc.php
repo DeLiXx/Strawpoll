6abaf52ef59d5bcbb6db5186393dfd420b96916f
Morice van Eckeveld <morice2@szut.uni-bremen.de>
2018-05-04 09:35:15 +0200

    Added email-handler and contact-form

---
+++ PhpStuff/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "den1.mysql6.gear.host";
+$dbUsername = "strawpoll";
+$dbPassword = "Ct4122a-v4~m";
+$dbName = "strawpoll";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
