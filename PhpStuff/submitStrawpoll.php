ca0bbfccc29cd08274230a9a6c1f408b7ca8b2bb
Paul Seibel <paul642@szut.uni-bremen.de>
2018-05-25 10:49:28 +0200

    submit test 17

--- PhpStuff/submitStrawpoll.php
+++ PhpStuff/submitStrawpoll.php
@@ -1,18 +1,19 @@
 <?php
+/*
 $x = fopen('autoIncrement.txt', 'rb');
 $y = fread($x, 9999);
 
 echo $y;
 fclose($x);
-/*
-$aIncrFile = fopen("autoIncrement.txt", "w");
-$aIncr = fread($aIncrFile,filesize("autoIncrement.txt"));
+*/
+$aIncrFile = fopen("autoIncrement.txt", "wb");
+$aIncr = fread($aIncrFile, 9999);
 if ($aIncr == "")  $aIncr = 0;
 $aIncr += 1;
 fwrite($aIncrFile, $aIncr);
 fclose($aIncrFile);
 
-$strawFile = fopen("../Strawpolls/" + $aIncr, "w");
+$strawFile = fopen("../Strawpolls/" + $aIncr, "wb");
 fclose($strawFile);
-*/
+
 ?>
