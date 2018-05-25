c395ec21074119c3a3e25f222dbc61c183af4a12
Paul Seibel <paul642@szut.uni-bremen.de>
2018-05-25 08:49:49 +0200

    new Strawpoll submit test

---
+++ PhpStuff/submitStrawpoll.php
@@ -0,0 +1,10 @@
+<?php
+$aIncrFile = fopen("autoIncrement.txt", "w") or die("Unable to open file!");
+$aIncr = fread($aIncrFile,filesize("autoIncrement.txt"));
+if ($aIncr == "") { $aIncr = 0};
+fwrite($aIncrFile, &aIncr + 1);
+fclose($aIncrFile);
+
+
+
+?>
