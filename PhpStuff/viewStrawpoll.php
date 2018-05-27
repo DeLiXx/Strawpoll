cf5e3a6ee38574a6a9361c70f544c3b2d362d8f1
DeLiXx <fake122344@gmail.com>
2018-05-27 17:56:54 +0200

    test

--- PhpStuff/viewStrawpoll.php
+++ PhpStuff/viewStrawpoll.php
@@ -9,17 +9,17 @@ fclose($fileHandle);
 $appendHtml = '
     <form class="form-horizontal formular">
             <fieldset>
-                <input name="title" class="form-control pollOption pollOptionHeader" placeholder="No Question" type="text" value=' . $args[0] . ' />
+                <input name="title" class="form-control pollOption pollOptionHeader" placeholder="No Question" type="text" value="' . $args[0] . '" />
                 <div class="form-group pollGroup">';
 
 for($x = 1; $x < sizeOf($args); $x++){
     $voteValues = explode(";", $args[$x]);
     $appendHtml .= '
     <div class="col-md-6">
-        <label value=' . $voteValues[0] . ' />
+        <label value="' . $voteValues[0] . '" />
     </div>
     <div class="col-md-2">
-        <label value=' . $voteValues[1] . ' class="jqVoteRow' . $x . '" />
+        <label value="' . $voteValues[1] . '" class="jqVoteRow' . $x . '" />
     </div>
     <div class="col-md-4">
         <input type="button" class="btn btn-info" onclick="incrementVote(' . $id . ',' . $x .'); return false;" />
