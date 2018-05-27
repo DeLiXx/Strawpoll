21be7239d90605b26e68ca950d55950ea5ab5275
DeLiXx <fake122344@gmail.com>
2018-05-27 12:06:01 +0200

    test

--- PhpStuff/submitStrawpoll.php
+++ PhpStuff/submitStrawpoll.php
@@ -13,8 +13,7 @@ fclose($aIncrFile);
 
 $strawFile = fopen("../Strawpolls/" + $aIncr + ".txt", "wb");
 fwrite($strawFile, $_GET["title"] + "/n");
-for($i=1; $i < $_GET["maxRow"]; $i++){
-    fwrite($strawFile, $_GET["selectionRow" + $i] + ";0/n")
-}
+for($i=1; $i < $_GET["maxRow"]; $i++)
+    fwrite($strawFile, $_GET["selectionRow" + $i] + ";0/n");
 fclose($strawFile);
 ?>
