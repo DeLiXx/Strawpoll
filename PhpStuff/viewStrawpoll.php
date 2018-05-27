c6447fa110a19f7d6376f53c4594329dfbc925c3
DeLiXx <fake122344@gmail.com>
2018-05-27 19:39:56 +0200

    fix viewStrawpoll layout

--- PhpStuff/viewStrawpoll.php
+++ PhpStuff/viewStrawpoll.php
@@ -9,20 +9,22 @@ echo $args[0];
 $appendHtml = '
     <form class="form-horizontal formular">
             <fieldset>
-                <input name="title" class="form-control pollOption pollOptionHeader" placeholder="No Question" type="text" value="' . $args[0] . '" readonly/>
+                <label name="title" class="form-control pollOption pollOptionHeader" placeholder="No Question" type="text" >' . $args[0] . '</label>
                 <div class="form-group pollGroup">';
 
-for($x = 1; $x < sizeOf($args); $x++){
+for($x = 1; $x < sizeOf($args) -1; $x++){
     $voteValues = explode(";;;", $args[$x]);
     $appendHtml .= '
-    <div class="col-md-6">
-        <label class="form-control pollOption">' . $voteValues[0] . '</label>
-    </div>
-    <div class="col-md-2">
-        <label class="jqVoteRow' . $x . '" >' . $voteValues[1] . '</label>
-    </div>
-    <div class="col-md-4">
-        <input type="button" value="Vote" class="btn btn-info" onclick="incrementVote(' . $id . ',' . $x .'); return false;" />
+    <div class="row">
+        <div class="col-md-6">
+            <label class="form-control pollOption">' . $voteValues[0] . '</label>
+        </div>
+        <div class="col-md-2">
+            <label class="jqVoteRow' . $x . '" >' . $voteValues[1] . '</label>
+        </div>
+        <div class="col-md-4">
+            <input type="button" value="Vote" class="btn btn-info" onclick="incrementVote(' . $id . ',' . $x .'); return false;" />
+        </div>
     </div>';
 }
 
