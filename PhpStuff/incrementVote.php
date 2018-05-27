15a9482f4a40583b2033055081ebeb0602f5fe74
DeLiXx <fake122344@gmail.com>
2018-05-27 21:41:38 +0200

    test incrementVote

--- PhpStuff/incrementVote.php
+++ PhpStuff/incrementVote.php
@@ -5,12 +5,13 @@
     $fileHandle = fopen("..\\Strawpolls\\"  . $id . ".txt", "rb");
     $fText = fread($fileHandle, 9999);
     
-    $args =unserialize (fread($fileHandle, 9999));
+    $args = unserialize (fread($fileHandle, 9999));
     fclose($fileHandle);
 
-    $args[$x * 2 + 1] += 1;
+    $voteValue = (int)$args[$x * 2 + 1];
+    $voteValue ++;
+    $args[$x * 2 + 1] = "" . $voteValue;
     $fileHandle = fopen("..\\Strawpolls\\"  . $id . ".txt", "wb");
     fwrite($fileHandle, serialize($args));
     fclose($fileHandle);
-    return NULL;
 ?>
